c5598473ea29d1ebae1b4c2b832104b343b20afd
ronysason <ronysason@gmail.com>
2018-04-21 22:59:37 +0300

    Send tap letter

---
+++ Assets/Scripts/Words/AnswerBox.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnswerBox : MonoBehaviour {
+
+    public Text letters;
+
+	// Use this for initialization
+	void Start () {
+        letters.text = "";
+	}
+
+    public void appendText (string letter) {
+        letters.text += letter;
+    }
+}
--- Assets/Scripts/Words/LetterDisplay.cs
+++ Assets/Scripts/Words/LetterDisplay.cs
@@ -6,8 +6,13 @@ using UnityEngine.UI;
 public class LetterDisplay : MonoBehaviour {
 
     public Text text;
+    public GameObject answerBox;
 
     public void SetLetter (string letter) {
         text.text = letter;
+    }
+
+    public void SendLetter () {
+        answerBox.GetComponent < AnswerBox > ().appendText(text.text);
     }
 }
