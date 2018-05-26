ce0e4277f1abef9eb77933fc7cdf8655bb435775
ronysason <ronysason@gmail.com>
2018-05-26 19:44:43 +0300

    end scene

---
+++ Assets/Scripts/UI/NewGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewGame : MonoBehaviour {
+
+    GameObject gameManager;
+
+	public void newGame ()
+    {
+        Destroy(GameManager.instance.gameObject);
+        SceneManager.LoadScene(1);
+    }
+}
