83536e97d40ec5f3b4805d07ac0ac0f4cb2dbe4d
ronysason <ronysason@gmail.com>
2018-05-26 11:58:58 +0300

    logo scene ready

---
+++ Assets/Scripts/Intro/LogoManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LogoManager : MonoBehaviour {
+
+	public GameObject Fadeout;
+	public float waitTime;
+	public int nextScene;
+
+
+	void Update () {
+
+		if (Input.anyKeyDown) {
+			Destroy (GameObject.Find ("LogoFadeIn"));
+			Fadeout.SetActive (true);
+			Debug.Log (Fadeout.GetComponent<CanvasGroup> ().alpha);
+
+            StartCoroutine("waitSeconds");
+
+			SceneManager.LoadScene(nextScene);
+		}
+			
+	}
+
+    IEnumerator waitSeconds()
+    {
+        yield return new WaitForSeconds(waitTime);
+    }
+}
